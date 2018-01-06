9091dc04fee2921a733997e6999448537b05b627
AlexMin314 <m98stone@gmail.com>
2018-01-06 16:21:58 +0800

    basic testing

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,9 @@ gulp.task('default', function() {
   // node source
   gulp.src('js/**/*.js')
     .pipe(babel())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist')) // save
+  // public source
+  gulp.src('public/js/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('public/dist'))
 })
